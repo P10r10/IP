583ebe18e8f4ff4e856fb66960752e52d781a792
P10r10 <70333265+P10r10@users.noreply.github.com>
2021-10-21 12:22:30 +0100

    Add files via upload

--- Semana3.java
+++ Semana3.java
@@ -15,23 +15,19 @@ public class Semana3{
 	}
 
 	static int largerDifferenceBetweenPrimes(int n){
-		int dif;
-		int maxDif = 0;
+		int dif = 0;
 		int i = 3;//só vamos testar a partir do primo 3
 		int lowPrime = 2;
 
-		if (n <= 2)
-			return maxDif;//zero pq ainda so temos um primo (2)
 		while (i <= n){
 			if (isPrime(i)){
-				dif = i - lowPrime;
+				if (dif < i - lowPrime)
+					dif = i - lowPrime;
 				lowPrime = i;
-				if (dif > maxDif)
-					maxDif = dif;
 			}
 			i++;
 		}
-		return maxDif;
+		return dif;
 	}
 
 	public static void main(String[] args) {
