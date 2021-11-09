6d13a69d94f0c82e2428f7e2af13e380817bafe7
P10r10 <70333265+P10r10@users.noreply.github.com>
2021-11-09 14:03:45 +0000

    Add files via upload

--- Semana3.java
+++ Semana3.java
@@ -30,6 +30,13 @@ public class Semana3{
 		return dif;
 	}
 
+	static int fibonacci(int n){
+		if (n == 0 || n == 1)
+			return n;
+		else
+			return fibonacci(n - 1) + fibonacci(n - 2);
+	}
+
 	public static void main(String[] args) {
 		Scanner input = new Scanner(System.in);
 		int i = 0;
@@ -45,5 +52,9 @@ public class Semana3{
 		System.out.print("\nThe largest difference between primes until "
 						 + a + " is ");
 		System.out.println(largerDifferenceBetweenPrimes(a) + ".");
+		System.out.println("\n\nInsert a positive integer: ");
+		int b = input.nextInt();
+		System.out.println("The fibonacci of " + b + " is " +
+			fibonacci(b));
 	}
 }
